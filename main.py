c23971846bf8b8466c6d6df08b1b2b422f9f5115
Scobra <79657875+ScobraScope@users.noreply.github.com>
2022-01-12 14:42:04 +0100

    Adding hunt, fish and dig

--- main.py
+++ main.py
@@ -20,13 +20,19 @@ except:
     print("No file with tokens detected. Please place a file in the same directory as the script called 'tokens.txt'")
 
 def sendmessage(token):
-    message = f"pls beg"
+    message = "pls beg"
+    messagedig = "pls dig"
+    messagefish = "pls fish"
+    messagehunt = "pls hunt
     message2 = f"random text to bypass the bot protection AAA {random.randint(19999,199999992323324324)}" #Sends a string with random numbers to trick dankmemer's macro detection
     pay = f"pls share max <@{id}>"
     headers = {'Authorization': token}
     while True:
         try:
             res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': message})
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagedig})
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagefish})
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagehunt})
             print(res)
 
             time.sleep(20 + random.randint(1,6))
@@ -34,6 +40,9 @@ def sendmessage(token):
             print(res)
             time.sleep(25 + random.randint(1,5))
             res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': message})
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagedig})
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagefish})
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagehunt})
             print(res)
 
             time.sleep(20 + random.randint(1,6))
@@ -41,6 +50,9 @@ def sendmessage(token):
             print(res)
             time.sleep(25 + random.randint(1,5))
             res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': message})
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagedig})
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagefish})
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagehunt})
             print(res)
 
             time.sleep(20 + random.randint(1,6))
@@ -48,13 +60,16 @@ def sendmessage(token):
             print(res)
             time.sleep(25 + random.randint(1,5))
             res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': message})
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagedig})
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagefish})
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagehunt})
             print(res)
 
             time.sleep(20 + random.randint(1,6))
             res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': message2  })
             print(res)
 
-            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': pay  })
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': pay})
             print(res)
             time.sleep(25 + random.randint(1,5))
             if res.status_code == 429:
