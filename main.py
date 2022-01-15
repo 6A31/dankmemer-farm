5374ed78c028ff0b4266c59338496eab767bc4ac
Scobra <79657875+ScobraScope@users.noreply.github.com>
2022-01-15 19:51:25 +0100

    Adding description and fixing the script

--- main.py
+++ main.py
@@ -7,16 +7,33 @@ from multiprocessing import Process
 #########################################
 
 channel_id = 'The id of your channel to farm in'
-id = 'Your id for receiving money'
+id = 'Your user id for receiving money'
 
 #########################################
 
+print("""
+https://github.com/ScobraScope/dankmemer-farm/
+
+This script is able to automatic farm dankmemer credits on multiple accounts
+at once. It sends post requests to discords API and can run in the background.
+Make sure to change the following variables in the python file before running it:
+
+channel_id
+id
+
+If the script is not working at all, feel free to contact me on discord, https://discord.gg/kjsdxkbtjw
+
+Made by 6A31 - https://github.com/scobrascope
+GNU GENERAL PUBLIC LICENSE V3
+
+Please follow the license at https://github.com/ScobraScope/dankmemer-farm/blob/main/LICENSE.md
+""")
 
 if channel_id == 'The id of your channel to farm in': 
     print("Make sure to specify a channel id. If you don't know how to obtain channel id's use Google.")
     time.sleep(5)
     quit()
-if channel_id == 'Your id for receiving money':
+if channel_id == 'Your user id for receiving money':
     print("Please specify what user to send the money to. specify a user id above. If you don't know how to obtain user id's use Google")
     time.sleep(5)
     quit()
@@ -45,13 +62,12 @@ if channel_id == 'Your id for receiving money':
 #╚█████╔╝██║░░██║██████╔╝███████╗
 #░╚════╝░╚═╝░░╚═╝╚═════╝░╚══════╝
 
-print("Starting...")
 
 try:
     token = open("tokens.txt", "r")
-    tokenlist = token.read().split("\n") #Creates a list with all the Tokens form the file
+    tokenlist = token.read().split("\n") #Creates a list with all the Tokens from the file
 except:
-    print("No file with tokens detected. Please place a file in the same directory as the script called 'tokens.txt'")
+    print("No file with tokens detected. Please place a file in the same directory as the script, called 'tokens.txt'")
 
 def sendmessage(token):
     message = "pls beg"
@@ -66,16 +82,28 @@ def sendmessage(token):
     while True:
         try:
             res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagehunt})
-            print("Hunt successfully")
+            if res.status_code == 200:
+                print("Hunted successfully")
+            else:
+                print(f"Failed to hunt. Errorcode {res}")
             time.sleep(5)
             res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': message})
-            print("beg successfully")
+            if res.status_code == 200:
+                print("Begged successfully")
+            else:
+                print(f"Failed to beg. Errorcode {res}")
             time.sleep(5)
             res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagefish})
-            print("fish successfully")
+            if res.status_code == 200:
+                print("Fished successfully")
+            else:
+                print(f"Failed to fish. Errorcode {res}")
             time.sleep(5)         
             res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagedig})
-            print("dig successfully")
+            if res.status_code == 200:
+                print("Fished successfully")
+            else:
+                print(f"Failed to fish. Errorcode {res}")
             time.sleep(5)
             time.sleep(random.randint(1,6))
 
@@ -85,7 +113,10 @@ def sendmessage(token):
             i += 1
             if i > 2:
                 res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': pay})
-                print("payout successfull")
+                if res.status_code == 200:
+                    print("Payout successful")
+                else:
+                    print(f"Failed to pay. Errorcode {res}")
                 i = 0
 
         except:
@@ -96,7 +127,6 @@ def sendmessage(token):
 
 #Start script
 if __name__=='__main__':
-    print("Starting threads...")
     for token in tokenlist:
         p1 = Process(target=sendmessage, args=(token,))
         p1.start()
